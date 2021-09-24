8f8edca08b686729fdfc914a7f2768927e104c75
ashish007kumar <ashishkumar357ak@gmail.com>
2021-09-25 00:10:39 +0530

    solve

---
+++ test.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+
+vector<int>ans;
+
+vector<int> allIndex(vector<int>& arr, int idx, int data, int count)
+{
+    if(idx==arr.size()){
+        return ans;
+    }
+    if(arr[idx]==data){
+        count++;
+        ans.push_back(idx);
+    }
+    return allIndex(arr,idx+1,data,count);
+}
+
+int main() {
+  int n ;
+  cin >> n;
+  vector<int> arr;
+  for (int i = 0; i < n; i++) {
+    int d;
+    cin >> d;
+    arr.push_back(d);
+  }
+  int data;
+  cin >> data;
+  vector<int> ans = allIndex(arr, 0, data, 0);
+  if (ans.size() == 0) {
+    cout << endl;
+    return 0;
+  }
+  for (int ele : ans) cout << ele << endl;
+
+  return 0;
+}
